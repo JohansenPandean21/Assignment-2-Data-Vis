5e9ec8204733229ab23da151d8a8c8af20f2f7f4
JohansenPandean21 <90506512+JohansenPandean21@users.noreply.github.com>
2021-09-23 21:55:00 +1000

    Delete melb_housing_price_unit.js

--- melb_housing_price_unit.js
+++
@@ -1,6 +0,0 @@
-var vg_1 = "js/melb_housing_price.vg.json"
-
-vegaEmbed("#choropleth_map",vg_1).then(function(result) { 
-
-    //Access the Vega view instance () as result.view
-}).catch(console.error);
