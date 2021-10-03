a7b9c1330962f39e496c5ab9ee66f1e08ab0918c
JohansenPandean21 <90506512+JohansenPandean21@users.noreply.github.com>
2021-10-03 21:36:56 +1100

    Add files via upload

--- js/melb_housing_price_unit.js
+++ js/melb_housing_price_unit.js
@@ -1,8 +1,15 @@
-
 var vg_1 = "js/melb_housing_price.vg.json";
 
 
 vegaEmbed("#choropleth_map",vg_1).then(function(result) { 
+
+    //Access the Vega view instance () as result.view
+}).catch(console.error);
+
+var vg_2 = "js/line.vg.json";
+
+
+vegaEmbed("#line_chart",vg_2).then(function(result) { 
 
     //Access the Vega view instance () as result.view
 }).catch(console.error);
